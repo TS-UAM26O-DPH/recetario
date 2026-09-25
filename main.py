7f2b531b9d3cdcb298539cf1e5a57e9bf2cf768c
Diego Peralta <144401211+PeraltaHD4K@users.noreply.github.com>
2026-09-25 16:15:26 -0600

    feat: agregar nueva funcionalidad

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from recetas import receta_pasta
+from recetas import receta_maruchan_suadero
 # Aquí se irán importando más recetas a medida que se agreguen
 
 def mostrar_menu():
     print("Recetario disponible:")
     print("1. Pasta al ajo")
+    print("2. Maruchan con Suadero")
     # Agrega aquí tu receta con un número nuevo
 
     opcion = input("Elige una receta (número): ")
 
     if opcion == "1":
         receta_pasta()
+    elif opcion == "2":
+        receta_maruchan_suadero()
     else:
         print("Opción no válida. Intenta de nuevo.")
 
